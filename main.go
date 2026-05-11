e2cf7b5ffdec4f7a272ef8fd82cdeb9e5e6b1e51
amr nashaat <amrnashaat0@gmail.com>
2026-05-12 02:56:12 +0300

    Move read line logic to getLinesChannel which spawns a goroutine for reading and returns a channel immediately the recieves the lines

--- main.go
+++ main.go
@@ -8,6 +8,50 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	// go routine that reads a line
+	go func() {
+		var line string
+		buf := make([]byte, 8)
+
+		for {
+			n, readErr := f.Read(buf)
+			if n > 0 {
+				tmp := string(buf[:n])
+				parts := strings.Split(tmp, "\n")
+				// if read string contains no new line "\n"
+				// we just concat the line string
+				if len(parts) == 1 {
+					line += parts[0]
+					continue
+				}
+				// if read string contains multiple new line character "\n"
+				// we add each part and print the whole line string and reset it, for each part except the last one we ignore it.
+				for i := range len(parts) {
+					if i == len(parts)-1 {
+						break
+					}
+					line = line + parts[i]
+					lines <- line
+					line = ""
+				}
+			}
+			if readErr != nil {
+				if errors.Is(readErr, io.EOF) {
+					close(lines)
+					f.Close()
+					return
+				} else {
+					panic(readErr)
+				}
+
+			}
+		}
+	}()
+
+	return lines
+}
 func main() {
 	f, err := os.Open("./message.txt")
 
@@ -15,41 +59,8 @@ func main() {
 		panic(fmt.Errorf("could not open file %w", err))
 	}
 
-	buf := make([]byte, 8)
-	var line string
-
-	for {
-		n, readErr := f.Read(buf)
-		if n > 0 {
-			tmp := string(buf[:n])
-
-			parts := strings.Split(tmp, "\n")
-			// if read string contains no new line "\n"
-			// we just concat the line string
-			if len(parts) == 1 {
-				line += parts[0]
-				continue
-			}
-			// if read string contains multiple new line character "\n"
-			// we add each part and print the whole line string and reset it, for each part except the last one we ignore it.
-			for i := range len(parts) {
-				if i == len(parts)-1 {
-					break
-				}
-				line = line + parts[i]
-				fmt.Printf("read: %s\n", line)
-				line = ""
-			}
-
-		}
-		if readErr != nil {
-			if errors.Is(readErr, io.EOF) {
-				fmt.Println("File ended!")
-				return
-			} else {
-				panic(readErr)
-			}
-
-		}
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Println("read: ", line)
 	}
 }
