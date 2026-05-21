2ca2e7f428e4e7cbd6454b5e71daed0e73128a2e
amrnashaat <amrnashaat0@gmail.com>
2026-05-21 04:06:49 +0300

    Modify RequestFromLine now it parses the chunks as they arrive instead of reading everything into memory first

--- request/request.go
+++ request/request.go
@@ -1,65 +1,71 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	"io"
-	"slices"
-	"strings"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	state       int // 1 initialized, 2 done
 }
-
 type RequestLine struct {
 	Method        string
 	RequestTarget string
 	HttpVersion   string
 }
 
-var InvalidMethodError error = fmt.Errorf("Invalid request method")
-var InvalidReqLineFormat error = fmt.Errorf("invalid request line format")
-var InvalidTargetError error = fmt.Errorf("Target in request line has invalid format")
-var InvalidHttpVersionError error = fmt.Errorf("Http version in request line has invalid format")
+func NewRequest() *Request {
+	return &Request{state: 1}
+}
+func (r *Request) parse(data []byte) (int, error) {
+	var nRead int
+	var err error
+	switch r.state {
+	case 1:
+		reqLine, n, parseErr := parseRequestLine(data)
+		if parseErr != nil {
+			return 0, parseErr
+		}
+		if n == 0 {
+			return 0, nil
+		}
+		nRead = n
+		err = parseErr
+		r.RequestLine = *reqLine
+		r.state = 2
+	case 2:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("error: unknown state")
 
-func parseRequestLine(line string) (*RequestLine, error) {
-	methods := []string{"GET", "POST", "DELETE", "PUT", "PATCH"}
-
-	parts := strings.Split(line, " ")
-	if len(parts) != 3 {
-		return nil, InvalidReqLineFormat
-	}
-	reqMethod := parts[0]
-	reqTarget := parts[1]
-	httpVer := parts[2]
-
-	if ok := slices.Contains(methods, reqMethod); !ok {
-		return nil, InvalidMethodError
-	}
-
-	if ok := strings.HasPrefix(reqTarget, "/"); !ok {
-		return nil, InvalidTargetError
-	}
-
-	if ok := strings.HasPrefix(httpVer, "HTTP/"); !ok {
-		return nil, InvalidHttpVersionError
 	}
 
-	httpVer = strings.TrimPrefix(httpVer, "HTTP/")
-	return &RequestLine{reqMethod, reqTarget, httpVer}, nil
+	return nRead, err
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	reqStr, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	readBuffer := make([]byte, 8)
+	acc := make([]byte, 0)
+	req := NewRequest()
+	for {
+		nRead, err := reader.Read(readBuffer)
+		acc = append(acc, readBuffer[:nRead]...)
+		if errors.Is(err, io.EOF) {
+			fmt.Println("reached eof")
+			break
+		}
+		n, parseErr := req.parse(acc)
+		if parseErr != nil {
+			fmt.Println(parseErr)
+			return nil, parseErr
+		}
+		if n == 0 {
+			continue
+		} else {
+			break
+		}
 	}
-
-	lines := strings.Split(string(reqStr), "\r\n")
-
-	reqLine, parseErr := parseRequestLine(lines[0])
-	if parseErr != nil {
-		return nil, parseErr
-	}
-	return &Request{*reqLine}, nil
+	return req, nil
 }
